3a36ccd177210f75caf84866563b28b44c5834e2
Andy Zhang <andzhng@gmail.com>
2017-11-19 22:57:25 -0800

    Small refactor

--- static/script.js
+++ static/script.js
@@ -1,24 +1,4 @@
 $(document).ready(() => {
-
-  let previewFile = () => {
-    let file = $('.file-input').prop('files');
-    if (file[0] && !(file[0].type === 'image/jpeg' || file[0].type === 'image/png')) {
-      alert('Need to upload an image.');
-      e.preventDefault();
-      return;
-    }
-    let reader = new FileReader();
-    reader.readAsDataURL(file[0]);
-    reader.onloadend = function(r) {
-      console.log(reader.result);
-      image.src = reader.result;
-      image.setAttribute('crossOrigin', 'anonymous');
-      image.onload = function() {
-        loadImage(ctx, image, 0.92);
-      };
-    };
-  };
-
   $('.file-input').change(() => {
     let file = $('.file-input').prop('files');
     if (file[0] && !(file[0].type === 'image/jpeg' || file[0].type === 'image/png')) {
